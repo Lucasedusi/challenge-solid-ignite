41295f3418f005c249baced2ccf4333588b8dfd4
lucasedusi <lucasmto.16@hotmail.com>
2022-04-19 16:58:24 -0300

    create UserController

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,13 +1,23 @@
+/* eslint-disable no-empty */
+/* eslint-disable prettier/prettier */
 import { Response, Request } from "express";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+	constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		const { name, email } = request.body;
+
+		try {
+			const user = this.createUserUseCase.execute({ name, email });
+
+			return response.status(201).json(user);
+		} catch (Error) {
+			return response.status(400).json({ error: Error });
+		}
+	}
 }
 
 export { CreateUserController };
