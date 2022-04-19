5cd6ed7a88d3f966fb7f3e9f83b998922cc61d88
lucasedusi <lucasmto.16@hotmail.com>
2022-04-19 17:12:27 -0300

    create UserAllController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,25 @@
+/* eslint-disable no-empty */
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+	constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		const { user_id } = request.params;
+
+		try {
+			const all = this.listAllUsersUseCase.execute({
+				user_id: String(user_id),
+			});
+
+			return response.json(all);
+		} catch (Error) {
+			return response.status(400).json({ error: Error });
+		}
+	}
 }
 
 export { ListAllUsersController };
