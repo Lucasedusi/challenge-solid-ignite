8efdebc32e2beeaabb947de7591db9e2d75c93b8
lucasedusi <lucasmto.16@hotmail.com>
2022-04-19 17:06:00 -0300

    create UserAdminController

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,13 +1,25 @@
+/* eslint-disable no-empty */
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+	constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		const { user_id } = request.params;
+
+		try {
+			const user = this.turnUserAdminUseCase.execute({
+				user_id: String(user_id),
+			});
+
+			return response.json(user);
+		} catch (Error) {
+			return response.status(404).json({ error: Error });
+		}
+	}
 }
 
 export { TurnUserAdminController };
