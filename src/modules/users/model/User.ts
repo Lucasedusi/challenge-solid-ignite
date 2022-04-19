e8a989aa54888d2a5db0edd6af34485149b0d5d7
lucasedusi <lucasmto.16@hotmail.com>
2022-04-19 13:37:12 -0300

    create user model

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+	id?: string;
+	name: string;
+	admin: boolean;
+	created_at: Date;
+	updated_at: Date;
+
+	constructor() {
+		this.admin = false;
+
+		if (!this.id) {
+			this.id = uuidV4();
+		}
+	}
 }
 
 export { User };
